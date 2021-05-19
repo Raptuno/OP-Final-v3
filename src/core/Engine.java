d1325870046a8d290e3739658f534de8b8bb1c69
Raptuno <65292196+Raptuno@users.noreply.github.com>
2021-05-19 07:49:49 -0500

    Initial commit

---
+++ src/core/Engine.java
@@ -0,0 +1,19 @@
+package core;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Engine {
+    public static List<String> HadasRescatadas = new ArrayList<>();
+    public static String[] nombreHada = { //No copié los nombres de las driadas de Terraria, no, para nada...
+            "Alalia",
+            "Alura",
+            "Ariella",
+            "Caelia",
+            "Calista"
+    }
+            ; // Fin nombres hadas
+    
+    public static int saludJugador = 100;
+    public static StringBuilder nombreJugador = new StringBuilder();
+}
---
+++ src/core/Main.java
@@ -0,0 +1,9 @@
+package core;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    
+    }
+}
